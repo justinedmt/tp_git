486f0f28c33191f5f1231dda79de94be3319db7c
Justine Desmedt <justinedesmedt@MacBook-Air-de-Justine.local>
2024-04-12 15:41:15 +0200

    modify addition

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 def addition(a, b):
-  return a+b
+  return a+b+a
 
 def soustraction(a,b):
   return a-b
@@ -9,6 +9,6 @@ def division(a,b):
   if b ==0:
     return("Error : This is not possible ")
   else:
-    return a/b 
+    return a/b
   
 
